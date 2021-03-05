0aba7b5140eaae9501274de3716f28530e56a025
Christian Mena <christian.91.gm@hotmail.com>
2021-03-05 16:08:39 -0500

    Created Header with rounded edges

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -9,3 +9,19 @@ class SquareHeader extends StatelessWidget {
     );
   }
 }
+
+class RoundedEdgeHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70)
+        )
+      ),
+    );
+  }
+}
