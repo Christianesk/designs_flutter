09a4f1496adeca4244bd9ed0daf50dfd7143ae12
Christian Mena <christian.91.gm@hotmail.com>
2021-03-08 16:58:08 -0500

    Design Peak Header using Custom painter

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -100,3 +100,46 @@ class _HeaderTrianglePainter extends CustomPainter {
     return true;
   }
 }
+
+
+class PeakHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _PeakHeaderPainter(),
+      ),
+    );
+  }
+}
+
+class _PeakHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //Properties
+
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth  = 20;
+
+
+    final path = new Path();
+
+    path.lineTo(0, size.height *0.30);
+    path.lineTo(size.width*0.5, size.height *0.35);
+    path.lineTo(size.width, size.height *0.30);
+    path.lineTo(size.width, 0);
+    // path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
