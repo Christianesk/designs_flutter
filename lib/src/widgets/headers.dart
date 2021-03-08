e4dbeca0f9919293794a8284a45a209db0cb0492
Christian Mena <christian.91.gm@hotmail.com>
2021-03-08 17:14:35 -0500

    Design Curve Header using Custom painter

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -143,3 +143,43 @@ class _PeakHeaderPainter extends CustomPainter {
     return true;
   }
 }
+
+class CurveHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _CurveHeaderPainter(),
+      ),
+    );
+  }
+}
+
+class _CurveHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //Properties
+
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth  = 20;
+
+
+    final path = new Path();
+
+    path.lineTo(0, size.height *0.25);
+    path.quadraticBezierTo(size.width*0.50, size.height *0.50, size.width, size.height *0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
