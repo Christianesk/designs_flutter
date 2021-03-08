23cf19ce623b3a02d5167fedb3ecabcfefe6224e
Christian Mena <christian.91.gm@hotmail.com>
2021-03-08 17:22:25 -0500

    Design Wave Header using Custom painter

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -183,3 +183,44 @@ class _CurveHeaderPainter extends CustomPainter {
     return true;
   }
 }
+
+class WaveHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _WaveHeaderPainter(),
+      ),
+    );
+  }
+}
+
+class _WaveHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //Properties
+
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth  = 20;
+
+
+    final path = new Path();
+
+    path.lineTo(0, size.height *0.25);
+    path.quadraticBezierTo(size.width*0.25, size.height *0.35, size.width*0.5, size.height *0.25);
+    path.quadraticBezierTo(size.width*0.75, size.height *0.15, size.width, size.height *0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
